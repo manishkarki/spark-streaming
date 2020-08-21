e54e8e5fce82a88feb90400354a8d2e5ff0b56c0
Manish Karki <mannishkarki@gmail.com>
2020-08-20 20:04:56 -0400

    inheritance and interface

--- src/main/scala/part1Recap/ScalaRecap.scala
+++ src/main/scala/part1Recap/ScalaRecap.scala
@@ -13,4 +13,16 @@ object ScalaRecap {
 
   // instructions vs expressions
   val theUnit = println("Hello, scala")
+
+  //oop
+  class Animal
+  class Dog extends Animal
+  trait Carnivore {
+    def eat(animal: Animal): Unit
+  }
+
+  class Crocodile extends Animal with Carnivore {
+    override def eat(animal: Animal): Unit =
+      println("Crunch!!")
+  }
 }
