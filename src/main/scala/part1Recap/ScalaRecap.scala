ce2669abfb821a212ceb7ee456a780103bf01007
Manish Karki <mannishkarki@gmail.com>
2020-08-20 20:02:51 -0400

    few recaps

--- src/main/scala/part1Recap/ScalaRecap.scala
+++ src/main/scala/part1Recap/ScalaRecap.scala
@@ -5,4 +5,12 @@ package part1Recap
   */
 object ScalaRecap {
 
+  // values and variables
+  val aBoolean: Boolean = false;
+
+  // expressions
+  val anIfExpression = if (2 > 3) "bigger" else "smaller"
+
+  // instructions vs expressions
+  val theUnit = println("Hello, scala")
 }
