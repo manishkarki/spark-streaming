c421997d736b537f81248e2d6faaa0dcff875025
Manish Karki <mannishkarki@gmail.com>
2020-11-12 19:54:49 -0500

    outline for KafkaIntegration

---
+++ src/main/scala/part4integrations/KafkaIntegration.scala
@@ -0,0 +1,27 @@
+package part4integrations
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * @author mkarki
+  */
+object KafkaIntegration {
+
+  val spark = SparkSession.builder()
+    .appName("Kafka integration")
+    .master("local[2]")
+    .getOrCreate()
+
+  def readFromKafka() = {
+    val kafkaDF = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "sparkstreaming")
+      .load()
+  }
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
